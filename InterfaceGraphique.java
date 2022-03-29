3de7c39fa66d01bec17a6835c2f90dc1272c7036
eeelsa <101322486+eeelsa@users.noreply.github.com>
2022-03-29 21:16:43 +0200

    Add files via upload

--- InterfaceGraphique.java
+++ InterfaceGraphique.java
@@ -1,16 +1,18 @@
 import java.awt.*;
 import java.awt.event.*;
 import java.io.File;
-
 import javax.swing.*;
 import java.util.Random;
 import java.awt.image.BufferedImage;
 import javax.imageio.ImageIO;
 import java.io.IOException;
+import javax.swing.SwingUtilities;
 
 
 public class InterfaceGraphique extends JFrame implements MouseListener {
+	
 	public JPanel principal;
+	
 	public JPanel p1;
 	public JPanel p2;
 	public JPanel p3;
@@ -23,84 +25,106 @@ public class InterfaceGraphique extends JFrame implements MouseListener {
 	public JTextField score;
 	public JTextField temps;
 	
-	public JLabel l1 = new JLabel("");
-	public JLabel img2;
-	
-	public int x;
-	public int y;
-	public int xF;
-	public int yF;
+	public JLabel l1;
+	public JLabel carte;
+
 	
 	public Bibliotheque b;
+	public Photo photoactuelle;
+	public int points = 0;
+	
+	private int x;
+	private int y;
+	public int xF;
+	public int yF;
+
+
 	
 	
 	public InterfaceGraphique(){
+		
 		super("geoguesseur");
-		
-		b=new Bibliotheque("photos");
-		
-		File carte = new File("carte2.JPG");
-						try{
-
-		BufferedImage bufferedImage2 = ImageIO.read(carte);
-		ImageIcon imageIcon2 = new ImageIcon(bufferedImage2);
-		ImageIcon scaledImage2 = new ImageIcon(imageIcon2.getImage().getScaledInstance(720, 360, Image.SCALE_DEFAULT));
-		img2 = new JLabel(imageIcon2);
-		img2.setIcon(scaledImage2);
-			} catch(IOException e){
-      System.out.println("Error: "+e);
-  }
-
-		
+		addMouseListener(this);  
 		this.setVisible(true);
-		this.setSize(1000,700);
+		this.setSize(900,700);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		int m = (int) ((Math.random() * (b.photos.size() - 1)) + 1);
+
+		p1 = new JPanel(new GridLayout(8,1));
 		p3 = new JPanel();
+		p2 = new JPanel(new GridLayout(2,1));
+		p4 = new JPanel();
 		
+		b=new Bibliotheque("LESPHOTOS");
+		File f = new File("carte2.jpg");
+
+		p2.add(p3);	
+		p2.add(p4);
+
+		
+		add(p2,BorderLayout.CENTER);
+		add(p1,BorderLayout.EAST);
+
+
+		try{
+			BufferedImage bufferedImage2 = ImageIO.read(f);
+			ImageIcon imageIcon2 = new ImageIcon(bufferedImage2);
+			ImageIcon scaledImage2 = new ImageIcon(imageIcon2.getImage().getScaledInstance(p4.getHeight(),p4.getWidth(), Image.SCALE_DEFAULT));
+			carte = new JLabel(imageIcon2);
+			carte.setIcon(scaledImage2);
+			} catch(IOException e){
+				System.out.println("Error: "+e);
+			}
+
+		p4.add(carte);
+
+		
+		int m = (int)((Math.random()*(b.photos.size()-1))+1);
 		int i=1;
 
 		for(Photo p : b.photos){		
 			if (i==m){
 				try{
-					System.out.println(i);
 					BufferedImage bufferedImage = ImageIO.read(p.file);
 					ImageIcon imageIcon = new ImageIcon(bufferedImage);
 					ImageIcon scaledImage = new ImageIcon(imageIcon.getImage().getScaledInstance(720, 360, Image.SCALE_DEFAULT));
 					JLabel img = new JLabel(imageIcon);
 					img.setIcon(scaledImage);
 					p3.add(img);
-			} catch(IOException e){
-      System.out.println("Error: "+e);
-  }
-    }
-    i++;
-		}
-		
-		p2= new JPanel(new GridLayout(2,1));
-		p2.add(p3);
-		p4= new JPanel();
-		p4.add(img2);
-		p2.add(p4);
-		p4.addMouseListener(this);  
+					photoactuelle = p;
+					p.utilise=true;
+					
+					} catch(IOException e){
+						System.out.println("Error: "+e);
+						}
+						}
+						i++;
+						}
 		
 		
 		// Panel droite
-		p1= new JPanel(new GridLayout(8,1));
-		l1.setText("Où a été prise cette photo ? Déplacer le curseur ");
+		l1 = new JLabel("Où a été prise cette photo ? Clique sur la carte ");
 		p1.add(l1);
+		
 		indice =new JButton("Obtenir un indice");
 		p1.add(indice);
+		indice.addActionListener(new EcouteurIndice(this));
+		
 		charger = new JButton("Je ne sais pas! Charger une nouvelle photo");
 		p1.add(charger);
+		charger.addActionListener(new EcouteurCharger(this));
+		
 		JLabel l2 = new JLabel("Total score");
 		p1.add(l2);
+		
 		score = new JTextField (5);
+		score.setEditable(false);
 		p1.add(score);
+		
 		JLabel l3 = new JLabel("Temps écoulé");
 		p1.add(l3);
+		
 		temps = new JTextField (5);
+		temps.setEditable(false);
 		p1.add(temps);
 		
 		valider = new JButton("Valider la position");
@@ -108,30 +132,28 @@ public class InterfaceGraphique extends JFrame implements MouseListener {
 		valider.addActionListener(new EcouteurValider(this));
 		
 		
-		add(p2,BorderLayout.CENTER);
-		add(p1,BorderLayout.EAST);
-		
-		
-			
-		
 	}
-	
 
 	public static void main (String[] args){
 		InterfaceGraphique i1 = new InterfaceGraphique();
+		
 	}
+	
 	public void paint(Graphics g){
         super.paint(g);
         g=p4.getGraphics();
         g.setColor(Color.BLUE);
         g.fillOval(x-5, y-5, 10, 10);
     }
-    public void mouseClicked(MouseEvent e) { 
-		x = e.getX();
-        y = e.getY();
-        repaint();      
-    }
 
+@Override
+    public void mouseClicked(MouseEvent e) { 
+		this.x = e.getX();
+        this.y = e.getY();        
+        
+        System.out.println(x+" - "+y);
+        repaint();  
+}
     
     public void mouseEntered(MouseEvent e) {  
         l1.setText("Mouse Entered");  
@@ -145,12 +167,69 @@ public class InterfaceGraphique extends JFrame implements MouseListener {
     public void mouseReleased(MouseEvent e) {  
         l1.setText("Mouse Released");  
     }  
-	
+    
 	public void ecouteValider(){
+        if(y>=p4.getHeight() && x<=p2.getWidth()){ //on pourrait rajouter quelque chose pour que si on clique en dehors, ca clique sur la meme ligne mais dans le panel
+
 		xF=x;
 		yF=y;
 		System.out.println(xF+" "+yF);
+		System.out.println(this.convertirX()+" - "+this.convertirY());
+		photoactuelle.compareGPS(this.convertirY(), this.convertirX());
+	}
 	}
 	
-	
+    
+    public void indice(){
+		JPanel zoneindice = new JPanel();
+		zoneindice.setBackground(new Color(0,100, 0, 50));
+		zoneindice.setBounds(1000, 100, 400, 400);
+		p3.add(zoneindice);
 }
+
+	public void charger(){
+		int m = (int)((Math.random()*(b.photos.size()-1))+1);
+		int i=1;
+		
+		for(Photo p : b.photos){		
+			if (i==m && p.utilise==false){ //gerer le fait que ça ne peut pas etre la meme
+				try{
+					System.out.println(i);
+					BufferedImage bufferedImage = ImageIO.read(p.file);
+					ImageIcon imageIcon = new ImageIcon(bufferedImage);
+					ImageIcon scaledImage = new ImageIcon(imageIcon.getImage().getScaledInstance(720, 360, Image.SCALE_DEFAULT));
+					JLabel img = new JLabel(imageIcon);
+					img.setIcon(scaledImage);
+					p3.removeAll();
+					p3.add(img);
+					photoactuelle = p;
+					p.utilise=true;
+					points = points - 10;
+					score.setText(""+points);
+					} catch(IOException e){
+						System.out.println("Error: "+e);
+						}
+						}
+						i++;
+						}
+						validate();
+						repaint();
+		
+	}
+	
+	public double convertirX(){
+		double aF = ( (double)xF - 8)/ (610 - 8 ) * ( 4.887425 - 4.868898 ) + 4.868898 ;
+														// xI est les coordones du point en haut a gauche et xO en bas a droite
+														// aI est les coordones en alt, latitude en haut a gauche et aO en bas a droite    
+				return aF;
+			}
+		
+	
+				
+		public double convertirY(){
+				double bF = ( (double)yF - 367 )/(367 - 685 ) * ( 45.788118 - 45.780469 ) + 45.788118;
+																																   
+				return bF;
+		}
+	}
+
--- Photo.java
+++ Photo.java
@@ -10,36 +10,38 @@ import java.io.*;
 public class Photo {
 	
 	public File file;
-	public int x;
-	public int y;
+	public double x;
+	public double y;
+	public boolean utilise;
 	
-	public Photo(File file, int x, int y){
+	public Photo(File file, double x, double y){
 		this.file=file;
 		this.x=x;
 		this.y=y;
+		this.utilise=false;
 	}
-	
-	public String compareGPS(int x2, int y2){
-		String s = "";
-		double eps1 = 1E-6;
-		double eps2 = 1E-3;
+		
+	public void compareGPS(double x2, double y2){
+		double eps1 = 1E-4;
+		double eps2 = 1E-2;
 		double eps3 = 1E-1;
 		
+		double distancex = (Math.abs(x2-this.x))*111195;
+		double distancey = (Math.abs(y2-this.y))*111195;
+		double distance = Math.sqrt((distancex)*(distancex)+(distancey)*(distancey));
+
+		
 		if(Math.abs(x2-this.x)<eps1 && Math.abs(y2-this.y)<eps1){
-			s="victoire";
-		}
-		if(Math.abs(x2-this.x)<eps2 && Math.abs(y2-this.y)<eps2){
-			s="pas mal";
-		}
-		if(Math.abs(x2-this.x)<eps3 && Math.abs(y2-this.y)<eps3){
-			s="un peu loin comem";
+			System.out.println(" clic : "+x2+" - "+y2+" - vraies coordonnées : "+this.x+" - "+this.y+" : victoire");
+		} else if(Math.abs(x2-this.x)<eps2 && Math.abs(y2-this.y)<eps2){
+			System.out.println(" clic : "+x2+" - "+y2+" - vraies coordonnées : "+this.x+" - "+this.y+" : pas mal");
+		}else if(Math.abs(x2-this.x)<eps3 && Math.abs(y2-this.y)<eps3){
+			System.out.println(" clic : "+x2+" - "+y2+" - vraies coordonnées : "+this.x+" - "+this.y+" : t'es loin");
 		} else {
-			s="non";
+			System.out.println(" clic : "+x2+" - "+y2+" - vraies coordonnées : "+this.x+" - "+this.y+" : t'es sur le meme continent bien joue bg");
 		}
-		return s;
+		System.out.println("tu es a "+distance);
 	}
 
 }
-
-
 
